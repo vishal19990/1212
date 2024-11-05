7d1a61543759a1a8ace2f928d77f383d9222731b
vishal19990 <vishalbangera98@gmail.com>
2024-11-05 22:26:53 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
   template: `
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
+      Learn more about Angularss
+      Learn more about Angularss
     </a>
   `,
 })
