61ff236b59fc09180b27ea2ac5b0f104fc0077b2
vishal19990 <vishalbangera98@gmail.com>
2024-11-05 22:23:48 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
+      Learn more about Angular
     </a>
   `,
 })
